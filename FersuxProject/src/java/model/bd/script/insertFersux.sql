9152276970bd7624ccc122a233d16300af3a8e0e
Jasleo <jasleo@protonmail.com>
2017-11-20 21:35:38 -0300

    Se agreharon dos usuarios

---
+++ FersuxProject/src/java/model/bd/script/insertFersux.sql
@@ -0,0 +1,5 @@
+USE fersux;
+
+INSERT INTO usuario VALUES
+(NULL, 'Diego','Hernriquez','diego@gmail.com','DiegoBailarin','1111','f','01-01-1799'),
+(NULL, 'Berna','Rdita','berny@gmail.com','Bernyy','2222','m','01-01-1499');
